2e29e63582cea0520f75e4806e57b6e1715d8939
araujocrk <ryansaopaulo2020@outlook.com>
2024-04-25 20:16:00 -0300

    just one ajustment

--- IFPI-Programação Estruturada de Computatores/Semana 10/Classroom + Runcodes/A2/Sem10_CR-A2-Q1.py
+++ IFPI-Programação Estruturada de Computatores/Semana 10/Classroom + Runcodes/A2/Sem10_CR-A2-Q1.py
@@ -1,6 +1,9 @@
-def main():
-    for num in range(99, 251):
+def repetir_musica(i,f):
+    for num in range(i, f):
         print((str(num)) + ' ' + 'bugs no software, pegue um deles e conserte...')
+
+def main():
+    repetir_musica(99, 251)
     
 if __name__ == '__main__':
     main()
