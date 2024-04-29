16b21c4471428ffedfc7fee639a3f7f9edd0b392
Ryan Araújo <167326341+araujocrk@users.noreply.github.com>
2024-04-29 10:28:41 -0300

    Update Sem10_CR-A2-Q1.py

--- Semana 10/Classroom + Runcodes/A2/Sem10_CR-A2-Q1.py
+++ Semana 10/Classroom + Runcodes/A2/Sem10_CR-A2-Q1.py
@@ -1,6 +1,6 @@
 def repetir_musica(i,f):
     for num in range(i, f):
-        print((str(num)) + ' ' + 'bugs no software, pegue um deles e conserte...')
+        print(str(num) + ' ' + 'bugs no software, pegue um deles e conserte...')
 
 def main():
     repetir_musica(99, 251)
