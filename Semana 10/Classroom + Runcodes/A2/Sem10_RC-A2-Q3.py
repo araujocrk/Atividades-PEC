6713d55c2201f566d9f1edfeedb13183181f905e
Ryan Araújo <167326341+araujocrk@users.noreply.github.com>
2024-04-29 10:32:55 -0300

    Update Sem10_RC-A2-Q3.py

--- Semana 10/Classroom + Runcodes/A2/Sem10_RC-A2-Q3.py
+++ Semana 10/Classroom + Runcodes/A2/Sem10_RC-A2-Q3.py
@@ -1,11 +1,10 @@
-def repetir_musica(i,f):
-    for num in range(i, f, 7):
+def repetir_musica(i, f, intv):
+    for num in range(i, f, intv):
         print((str(num)) + ' ' + 'bugs no software, pegue sete deles e conserte...')
         print('Tecle "Ctrl+F5"')
-
-def main():
-    repetir_musica(99, 251)
     print(f'Vamos fazer mais um café!')
+def main():
+    repetir_musica(99, 251, 7)
     
 if __name__ == '__main__':
     main()
